603f53b5b4524326f2b4c3917998753476668613
Richard Horrocks <richardjhorrocks@gmail.com>
2017-04-22 11:39:39 +0100

    First batch of tools.

---
+++ ethereum_checkTransactionCount.js
@@ -0,0 +1,12 @@
+function checkTransactionCount(startBlockNumber, endBlockNumber) {
+  console.log("Searching for non-zero transaction counts between blocks "  + startBlockNumber + " and " + endBlockNumber);
+
+  for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+    var block = eth.getBlock(i);
+    if (block != null) {
+      if (block.transactions != null && block.transactions.length != 0) {
+        console.log("Block #" + i + " has " + block.transactions.length + " transactions")
+      }
+    }
+  }
+}
