105fb7899219511eb1410095ebd0f0458705c0e3
Richard Horrocks <richardjhorrocks@gmail.com>
2018-02-19 11:00:01 +0000

    Create ethereum_HdWalletFromMnemonic.js

---
+++ ethereum_HdWalletFromMnemonic.js
@@ -0,0 +1,10 @@
+const bip39 = require('bip39');
+const hdkey = require('ethereumjs-wallet/hdkey');
+
+const mnemonic = '..';
+const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+const path = "m/44'/60'/0'/0/0";
+const wallet = hdwallet.derivePath(path).getWallet();
+const address = `0x${wallet.getAddress().toString('hex')}`;
+
+console.log(`Address: ${address}`);
